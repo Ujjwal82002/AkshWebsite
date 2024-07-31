a58c333c416e9a7ddde4801620d79a355a0e6c9a
Sumit Pandey <148738250+sumitpandey1999@users.noreply.github.com>
2024-07-31 16:22:05 +0530

    servicepage1 and 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Display from "./Display";
-
-
-
+import ServicesPage1 from "./components/ServicesPage1";
 
 export default function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Display />}/>
-    
+      <Route path= "/servicespage1" element={<ServicesPage1/>}/>
     </Routes>
    </BrowserRouter>
 
