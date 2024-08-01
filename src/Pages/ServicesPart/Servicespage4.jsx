93e47c6a88964479b8a08de579373b8e89657065
Sumit Pandey <148738250+sumitpandey1999@users.noreply.github.com>
2024-08-01 15:21:37 +0530

    service pages add

---
+++ src/Pages/ServicesPart/Servicespage4.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import contactus from "../../assets/akshtech images/contactus.svg";
+import servicepage4design from "../../assets/akshtech images/servicepage4design.svg";
+
+const Servicespage4 = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen text-white p-4">
+      <div className="w-full max-w-6xl bg-[#000116] rounded-xl flex flex-col lg:flex-row justify-between items-center p-4 lg:p-10">
+        <div className="flex flex-col justify-center items-center lg:items-start mt-10 lg:mt-0">
+          <h1 className="text-3xl font-bold w-full lg:w-[573px] h-auto lg:h-[104px] text-center lg:text-left">
+            Top UI/UX Design Company
+          </h1>
+          <p className="w-full lg:w-[500px] h-auto lg:h-[128px] mt-4 lg:-mt-10 text-center lg:text-left">
+            Our Company specializes in bridging the gap between user
+            expectations. Our designers utilize cutting-edge tools like Figma,
+            Framer, Adobe XD, and Wix Studio to create data-driven designs{" "}
+            <br /> that align with technological advancements.
+          </p>
+          <button className="flex bg-[#FFFFFF] w-full lg:w-96 justify-between text-black items-center pl-4 text-lg font-bold rounded-lg mt-4 lg:mt-10">
+            Contact Us
+            <img src={contactus} alt="contactus" />
+          </button>
+        </div>
+        <div className="mt-10 lg:mt-0">
+          <img src={servicepage4design} alt="servicepage4design" className="w-full lg:w-auto" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Servicespage4;
